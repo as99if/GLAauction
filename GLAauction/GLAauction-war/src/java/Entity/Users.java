7d68c53132edebb5c97252854d8ef225df8caceb
Asif Shuvo <as99if@users.noreply.github.com>
2021-02-16 17:13:13 +0100

    Update Users.java

--- GLAauction/GLAauction-war/src/java/Entity/Users.java
+++ GLAauction/GLAauction-war/src/java/Entity/Users.java
@@ -19,7 +19,7 @@ import javax.persistence.Table;
  */
 @Entity
 @Table
-public class User implements Serializable {
+public class Users implements Serializable {
 
     private static final long serialVersionUID = 1L;
     @Id
